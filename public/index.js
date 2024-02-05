9092e21036f228cabee787773ff5111a04556bee
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-02-05 23:14:52 +0100

    recupera risposte

--- public/index.js
+++ public/index.js
@@ -8,25 +8,36 @@ let domandaTemplate = `
   <p> -> <span class="fw-bold mt-5">%DOMANDA</span></p>
   <form method="" action="">
   <div class="form-check">
-    <input name="gruppo1" type="radio" id="radio1" value="%VALUE1">
+    <input name="gruppo" class="%CERCA" type="radio" id="radio1" value="%VALUE1">
     <label for="radio1">%RADIO1</label>
   </div>
   <div class="form-check">
-    <input name="gruppo1" type="radio" value="%VALUE2"  id="radio2" >
+    <input name="gruppo" class="%CERCA" type="radio" value="%VALUE2"  id="radio2" >
     <label for="radio2">%RADIO2</label>
   </div>
     <div class="form-check">
-    <input name="gruppo1" type="radio" value="%VALUE3"  id="radio3" >
+    <input name="gruppo" class="%CERCA" type="radio" value="%VALUE3"  id="radio3" >
     <label for="radio3">%RADIO3</label>
   </div>
     <div class="form-check">
-    <input name="gruppo1" type="radio" value="%VALUE4"  id="radio4" >
+    <input name="gruppo" class="%CERCA" type="radio" value="%VALUE4"  id="radio4" >
     <label for="radio4">%RADIO4</label>
   </div>
   </form>
 `;
 let partita = {
 }
+
+/**
+ * Funzione per la gestione del tempo
+ * @param {*} time 
+ * @returns 
+ */
+const timer = (time) =>{
+    return setTimeout(window.location.reload(),time);
+}
+
+
 /**
  * Funzione per recuperare da server tutte le persone che hanno fatto il quiz
  * @returns promise
@@ -40,6 +51,10 @@ const recuperaPersone = () =>{
     });
 }
 
+/**
+ * Funzione per recuperare da server le domande da fare all'utente
+ * @returns promise
+ */
 const recuperaDomande = () =>{
     return new Promise((resolve,reject)=>{
         fetch("/questions")
@@ -83,20 +98,37 @@ confermaNome.onclick = () =>{
     if(nome.value){
         partita = {};
         partita['username'] = nome.value;
+        nome.value = "";
        recuperaDomande().then(response => {
         const domande = response.questions;
         let html = "";
         for(let i=0;i<domande.length;i++){
             let riga = domandaTemplate;
             riga = riga.replace("%DOMANDA",domande[i].question);
+            console.log(domande[i].answers);
             for(let j=0;j<domande[i].answers.length;j++){
-                console.log(domande[i].answers[j]);
-                riga = riga.replace("%RADIO"+(j+1),domande[i].answers[j]);
+                riga = riga.replace("%RADIO"+(j+1),domande[i].answers[j]).replace("%RADIO"+(j+1),j).replace("%CERCA","input");
             }
-            html+=riga;
+            html += riga;
         }
-        domandeDiv.innerHTML = html;
-       });
+        domandeDiv.innerHTML = html+'<div class="row justify-content-end><div class="col-auto"><input type="button" id="confermaRisposte" class="btn btn-success mt-3" value="Conferma"/></div></div>';
+        //gestione risposte
+        document.getElementById("confermaRisposte").onclick = () =>{
+            const inputs = document.querySelectorAll(".input");
+            const risposteDate = [];
+            let conta = 0;
+            for(let i=0;i<inputs.length;i++){
+                if(inputs[i].checked){
+                    const json = {};
+                    json[conta] = inputs[i].id.replace("radio","")
+                    risposteDate.push(json);
+                    conta ++;
+                }
+            }
+            partita['timestamp'] = "";//non so cosa inserire
+            partita['answers'] = risposteDate;
+        }
+    });
     }
 }
 
